9f97b71a0b7c6621e1305efb1666f3a37934c4aa
Erfan Eslami <erfan.88.eslami@gmail.com>
2025-07-26 21:51:56 +0330

    feat: add average of sub tree

---
+++ CodeChallenges/Problems/AverageOfSubTree/AverageOfSubTree.cs
@@ -0,0 +1,42 @@
+﻿using CodeChallenges.Problems.AverageOfSubTree.Models;
+
+namespace CodeChallenges.Problems.AverageOfSubTree;
+
+public class AverageOfSubTree
+{
+   private static int ResultNodeCount { get; set; }
+   
+   public static int Solve(TreeNode root)
+   {
+      CalcNode(root);
+      return ResultNodeCount;
+   }
+
+   private static (int sum, int count) CalcNode(TreeNode node)
+   {
+      var sum = node.val;
+      var count = 1;
+      
+      if (node.left != null)
+      {
+         var calcNode = CalcNode(node.left);
+         sum += calcNode.sum; 
+         count += calcNode.count;
+      }
+      
+      if (node.right != null)
+      {
+         var calcNode = CalcNode(node.right);
+         sum +=  calcNode.sum;
+         count += calcNode.count; 
+      }
+      
+      var average = sum / count;
+      if (average == node.val)
+      {
+         ResultNodeCount++;
+      }
+      
+      return (sum, count);
+   }
+}
--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace CodeChallenges;
+﻿using CodeChallenges.Problems.AverageOfSubTree;
+using CodeChallenges.Problems.AverageOfSubTree.Models;
+
+namespace CodeChallenges;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        var testData = new TreeNode()
+        {
+            val = 1
+        };
+
+        AverageOfSubTree.Solve(testData);
     }
 }
