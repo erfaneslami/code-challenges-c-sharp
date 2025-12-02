97ade0f9af59d704c1fcddddd9c21a1d777a9396
erfan <erfan.88.eslami@gmail.com>
2025-12-02 20:02:38 +0330

    i don't know

---
+++ CodeChallenges/LeetCode/486_Predict_the_Winner/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace CodeChallenges.LeetCode._486_Predict_the_Winner;
+
+    public class GameState
+    {
+        public int[] CurrentBoard { get; set; }
+        public pTYPE Type { get; set; }
+    }
+
+public sealed class Solution {
+    public bool PredictTheWinner(int[] nums)
+    {
+
+        return true;
+    }
+
+
+    private void BFSTraverseTree(int[] nums)
+    {
+        var turn = 1;
+        var p1Scores = 0;
+        var p2Scores = 0;
+        var queue = new Queue<int>();
+        var counter = 0; 
+        queue.Enqueue(nums[0]);
+
+        while (queue.Count > 0)
+        {
+            var score = queue.Dequeue();
+            if (turn == 1)
+            {
+                p1Scores += score;
+                turn = 2;
+            }
+            else
+            {
+                p2Scores += score;
+                turn = 1;
+            }
+            
+            queue.Enqueue(nums[counter]);
+            queue.Enqueue(nums[nums.Length - 1 - counter]);
+            counter++;
+            
+        }
+    }
+
+}
--- CodeChallenges/Program.cs
+++ CodeChallenges/Program.cs
@@ -1,5 +1,6 @@
 ﻿
-using CodeChallenges.LeetCode._198_House_Robber;
+
+using CodeChallenges.LeetCode._486_Predict_the_Winner;
 
 namespace CodeChallenges;
 
@@ -9,7 +10,7 @@ class Program
     {
         Console.WriteLine("Hello, World!");
 
-        var result = new Solution().Rob([226,174,214,16,218,48,153,131,128,17,157,142,88,43,37,157,43,221,191,68,206,23,225,82,54,118,111,46,80,49,245,63,25,194,72,80,143,55,209,18,55,122,65,66,177,101,63,201,172,130,103,225,142,46,86,185,62,138,212,192,125,77,223,188,99,228,90,25,193,211,84,239,119,234,85,83,123,120,131,203,219,10,82,35,120,180,249,106,37,169,225,54,103,55,166,124]);
+        var result = new Solution().PredictTheWinner([3606449,6,5,9,452429,7,9580316,9857582,8514433,9,6,6614512,753594,5474165,4,2697293,8,7,1]);
         Console.WriteLine(result);
     }
 
